a92db65d2d228838035bc180d0ba2308b7b463a2
Vrinda <vrindasiddhpura11@gmail.com>
2026-08-09 16:13:35 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class P1{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Mark1: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter Mark2: ");
+        int b = sc.nextInt();
+
+        System.out.print("Enter Mark3: ");
+        int c = sc.nextInt();
+
+        System.out.print("Enter Mark4: ");
+        int d = sc.nextInt();
+
+        System.out.print("Enter Mark5: ");
+        int e = sc.nextInt();
+
+        double res = (a+b+c+d+e)/5;
+
+        if(res>=60){
+            System.out.print("First Divison.");
+        }
+        else if(res>=50 && res<=59){
+            System.out.print("Second Divison.");
+        }
+        else if(res>=40 && res<=49){
+            System.out.print("Third Divison.");
+        }
+        else{
+            System.out.print("Fail.");
+        }
+    }
+}
