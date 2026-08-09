779c773fb3ed817820e36c7179045d45c965930a
Vrinda <vrindasiddhpura11@gmail.com>
2026-08-09 16:15:17 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class P1{
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        int count = 0;
+        for(int i=1 ; i<=n ; i++){
+            if(n%i==0){
+                count++;
+            }
+        }
+
+        if(count==2){
+            System.out.print("Prime Number.");
+        }
+        else{
+            System.out.print("Not Prime Number.");
+        }
+    }
+}
