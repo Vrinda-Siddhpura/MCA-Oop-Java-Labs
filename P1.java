9f790afad9e938afc47f8282c0fedddebe96a9cf
Vrinda <vrindasiddhpura11@gmail.com>
2026-08-09 16:09:17 +0530

    Add files via upload

---
+++ P1.java
@@ -0,0 +1,11 @@
+public class P1{
+    public static void main(String []args){
+        System.out.println(""+ (10 + 20 * 30));
+        System.out.println(""+ (100 + 200 / 10 - 3 * 10));
+        System.out.println(""+ (50 * 4 + 80 / 8 - 5));
+        System.out.println(""+ (100 + 200 / 10 - 3 * 10));
+        System.out.println(""+ (400 - 50 * 3 + 90 / 9));
+        System.out.println(""+ (250 + 100 / 20 * 6 - 30));
+        System.out.println(""+ (250 - 100 / (5 + 5) + 12 * 3));
+    }
+}
